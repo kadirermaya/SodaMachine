ce164caecd995c8c7710b6cb72bead7fa1d85420
kadirermaya <kadir.ermaya@gmail.com>
2020-06-16 10:59:41 -0700

    classes are created

---
+++ SodaMachineProject/Cola.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachineProject
+{
+    class Cola
+    {
+        // MEMBER VARIABLES (HAS A)
+        // CONSTRUCTOR (SPAWNER)
+        // MEMBER METHODS (CAN DO)
+    }
+}
