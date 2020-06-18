c5e355441477a9b877051adf58fcd8b7592fc3ae
kadirermaya <kadir.ermaya@gmail.com>
2020-06-18 11:47:25 -0700

    Static User Interface Class created

---
+++ SodaMachineProject/StaticUserInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachineProject
+{
+    static class StaticUserInterface
+    {
+        public static void Write (string stringEntry)
+        {
+            Console.WriteLine(stringEntry);
+            
+        }
+    }
+}
